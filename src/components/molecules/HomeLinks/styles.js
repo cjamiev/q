b51b2d7d04c2ff5fcfd50fe571ad4780dcd0e782
Chris Varughese <cjamiev@gmail.com>
2025-08-02 13:49:45 -0500

    feat: updating HomeLinks as it is moved to backlog repo and using it to traverse youtube songs

--- src/components/molecules/HomeLinks/styles.js
+++ src/components/molecules/HomeLinks/styles.js
@@ -1,75 +1,68 @@
 import styled from 'styled-components';
 
-export const SCTabWrapper = styled.div`
+export const SCMusicCardWrapper = styled.form`
   display: flex;
-  flex-wrap: wrap;
-  `;
-
-export const SCLinkSectionWrapper = styled.div`
-  flex: 5;
-  display: flex;
-  flex-wrap: wrap;
-  margin: 3px;
+  flex-direction: column;
+  padding: 16px;
+  width: 300px;
+  height: 250px;
+  border: 1px solid black;
+  border-radius: 10px;
 `;
 
-export const SCFavoriteLinkWrapper = styled.div`
+export const SCMusicCardLabel = styled.label`
+  color: black;
+  font-weight: 500;
+`;
+
+export const SCMusicCardLink = styled.a`
+  right: 15px;
+  bottom: 15px;
+  padding: 10px;
+  background: white;
+  font-weight: 600;
+  text-align: center;
+  place-content: center;
+  border-radius: 5px;
+  color: black;
+  background: white;
+
+  &:hover {
+    outline: 1px solid black;
+    background: white;
+    color: black;
+  }
+`;
+export const SCMusicCardInput = styled.input`
+  width: 90%;
+  padding: 10px;
+  border-radius: 6px;
+  border: 1px solid #e0e7ff;
+  margin-top: 4px;
+  margin-bottom: 15px;
+  font-size: 15px;
+
+  &:focus {
+    outline: none;
+    border-color: #3b82f6;
+    box-shadow: 0 0 0 3px rgba(59, 130, 246, 0.1);
+  }
+`;
+export const SCMusicCardSubmit = styled.button`
+  padding: 10px 0;
+  border-radius: 6px;
+  border: none;
+  background: black;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
   cursor: pointer;
-`;
+  box-shadow: 0 1px 4px rgba(79, 70, 229, 0.08);
+  transition: background-color 0.2s;
 
-export const SCSongAndSearchSectionWrapper = styled.div`
-  flex: 3;
-  margin: 3px;
-`;
-
-export const SCListWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-export const SCNextLinkBtnWrapper = styled.div`
-  display: block;
-  margin-bottom: 10px;
-
-  button {
-    width: 250px;
-  }
-`;
-
-export const SCLinkCardWrapper = styled.div`
-> div {
-    background-color: #e1dfdf;
-    height: 50px;
-    width: 300px;
-  }
-
-  > div > div {
-    text-align: center;
-    margin: 0;
-  }
-`;
-
-export const SCSongAndSearchCardWrapper = styled.div`
-> div {
-    background-color: ${(props) => (props.isAlreadyVisited ? 'black' : 'white')};
-    height: 40px;
-    width: 180px;
-  }
-
-  > div > div {
-    text-align: center;
-    margin: 0;
-  }
-`;
-
-export const SCCountWrapper = styled.div`
-  display: flex;
-  margin-bottom: 5px;
-
-  button {
-    width: 80px;
-    min-height: unset;
-    height: 25px;
-    font-size: 14px;
-    margin-left: 5px;
+  &:hover {
+    border: 1px solid black;
+    background: white;
+    color: black;
   }
 `;

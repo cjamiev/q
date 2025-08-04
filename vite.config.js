b9e69fc2a78ed7c7c94bd73fbf9bee60c4e8a757
Chris Varughese <cjamiev@gmail.com>
2025-08-03 20:12:18 -0500

    feat: updating ports

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,26 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 4500,
     proxy: {
       '/db': {
-        target: 'http://localhost:2000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
       '/project': {
-        target: 'http://localhost:2000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
       '/file': {
-        target: 'http://localhost:2000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
       '/command': {
-        target: 'http://localhost:2000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
       },
       '/library': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:4010',
         changeOrigin: true,
       }
     }

b9fcdb655ef85e987d1e198258bbe25ffd09482a
Chris Varughese <cjamiev@icloud.com>
2026-06-28 16:17:37 -0500

    feat: adding backup btn

--- src/components/pages/Clipboard/Clipboard.jsx
+++ src/components/pages/Clipboard/Clipboard.jsx
@@ -2,9 +2,22 @@ import Page from '../../layout';
 import { Clips } from './Clips';
 import { Timer } from './Timer';
 import { Links } from './Links';
+import * as ALL_STORAGE from '../../../constants/localstorage';
+import { copyToClipboard } from '../../../utils/copy';
 import './clipboard.css';
 
 const Clipboard = () => {
+  const handleBackup = () => {
+    const allItems = [];
+    for (const key of Object.keys(ALL_STORAGE)) {
+      allItems.push(ALL_STORAGE[key]);
+    }
+    const allValues = allItems.map((item) => {
+      return { key: item, value: localStorage.getItem(item) };
+    })
+    copyToClipboard(JSON.stringify(allValues));
+  }
+
   return (
     <Page>
       <div className='clipboard-sections'>
@@ -12,6 +25,7 @@ const Clipboard = () => {
         <Timer />
         <Links />
       </div>
+      <button className='clipboard-backup-btn' onClick={handleBackup}>Backup</button>
     </Page>
   );
 };
